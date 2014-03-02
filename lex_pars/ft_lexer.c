c6168a67f9fa6e311c124e25201eafb6ff33c756
Brahim BOUABOUD <bbouabou@e1r5p10.42.fr>
2014-03-02 11:18:38 +0100

    lexer / parser regler vu que antoine ne bouge pas son cul

--- lex_pars/ft_lexer.c
+++ lex_pars/ft_lexer.c
@@ -6,7 +6,7 @@
 /*   By: amorfan <amorfan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/12 15:31:04 by amorfan           #+#    #+#             */
-/*   Updated: 2014/02/28 13:11:58 by mmartin          ###   ########.fr       */
+/*   Updated: 2014/03/02 11:17:55 by bbouabou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,43 @@ static const t_token	tok[10] =
 	{'\0', 0, 0}
 };
 
+static int				ft_check_tok(char c)
+{
+	int			i;
+
+	i = 0;
+	while (tok[i].type != 0)
+	{
+		if (c == tok[i].c)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static char				*ft_search_word(char **str, int i, char c)
+{
+	int			j;
+	char		*tmp;
+
+	if ((*str)[i] && (*str)[i] == c)
+		i++;
+	if ((*str)[i] && (*str)[i] == c)
+		i++;
+	j = i;
+	while ((*str)[j] == ' ')
+		j++;
+	while ((*str)[j] != ' ' && ft_check_tok((*str)[j]) && (*str)[j])
+		j++;
+	tmp = ft_strndup((*str) + i, j - i);
+	while (i < j)
+	{
+		(*str)[i] = ' ';
+		i++;
+	}
+	return (tmp);
+}
+
 void					ft_lexer(t_lexer **root, char *str)
 {
 	int			i;
@@ -43,14 +80,35 @@ void					ft_lexer(t_lexer **root, char *str)
 	}
 	if (*root == NULL && !str[i])
 		return ;
-	ft_add_lexer(root, ft_strndup(str, i), 0, 5);
-	if (str[i] && str[i + 1] && str[i] == str[i + 1] && tok[j].type <= 7)
+	if (tok[j].rank == 4)
 	{
-		j++;
-		ft_add_lexer(root, ft_strndup(str + i++, 2), tok[j].type, tok[j].rank);
+		ft_add_lexer(root, ft_search_word(&str, i, tok[j].c), 0, 5);
+		if (str[i] && str[i + 1] && str[i] == str[i + 1])
+		{
+			j++;
+			ft_add_lexer(root, ft_strndup(str + i++, 2), tok[j].type, tok[j].rank);
+			str[i - 1] = ' ';
+			str[i] = ' ';
+		}
+		else if (str[i])
+		{
+			ft_add_lexer(root, ft_strndup(str + i, 1), tok[j].type, tok[j].rank);
+			str[i] = ' ';
+		}
+		if (str[i])
+			ft_lexer(root, str);
 	}
-	else if (str[i] && tok[j].type > 0)
-		ft_add_lexer(root, ft_strndup(str + i, 1), tok[j].type, tok[j].rank);
-	if (str[i] && *(str + i + 1))
-		ft_lexer(root, str + i + 1);
+	else
+	{
+		ft_add_lexer(root, ft_strndup(str, i), 0, 5);
+		if (str[i] && str[i + 1] && str[i] == str[i + 1] && tok[j].type <= 7)
+		{
+			j++;
+			ft_add_lexer(root, ft_strndup(str + i++, 2), tok[j].type, tok[j].rank);
+		}
+		else if (str[i] && tok[j].type > 0)
+			ft_add_lexer(root, ft_strndup(str + i, 1), tok[j].type, tok[j].rank);
+		if (str[i] && *(str + i + 1))
+			ft_lexer(root, str + i + 1);
+	}
 }
